b1fd12e49a560f3b6b685081a84aa53ff993523f
Gopall Borse <gopallborse@gmail.com>
2025-03-29 16:54:39 +0530

    meals database added, loading, error, not found pages added

--- 02-foodies/app/meals/page.js
+++ 02-foodies/app/meals/page.js
@@ -1,7 +1,14 @@
 import Link from "next/link";
-import classes from "./page.module.css";
+import { Suspense } from "react";
 
+import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
@@ -23,7 +30,11 @@ export default function MealsPage() {
       </header>
 
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
